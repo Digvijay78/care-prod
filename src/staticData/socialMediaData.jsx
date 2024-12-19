7ef947630b76b59518826cf19b8ce9a54b4764af
Digivjay <digvijaysingh@psymate.org>
2024-12-19 19:52:26 +0530

    asads

---
+++ src/staticData/socialMediaData.jsx
@@ -0,0 +1,14 @@
+export const socialData = [
+    {
+        "socialUrl": "#",
+        "socialIcon": "fab fa-facebook-f"
+    },
+    {
+        "socialUrl": "#",
+        "socialIcon": "fab fa-twitter"
+    },
+    {
+        "socialUrl": "#",
+        "socialIcon": "fab fa-google"
+    }
+]
